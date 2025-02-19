12b3fdad4a1c04b46c51079981173897342f221e
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2025-02-19 11:46:29 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT
 
 const allowedOrigins = [
     "http://localhost:5173", 
-    "https://expense-tracker-sg1z.vercel.app" 
+    "https://expense-tracker-ea8j.onrender.com" 
 ];
 
 //middLewares
