36a6a9c6b09db605f99a3fb6237fab75525f5e72
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2025-02-19 10:36:28 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,26 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 
+const allowedOrigins = [
+    "http://localhost:5173", 
+    "https://expense-tracker-sg1z.vercel.app" 
+];
+
 //middLewares
 
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
